d55f1c8fb13cd135679932cd48a85e974922ca79
JustinDanielaBahamon <dannybahamon613@gmail.com>
2026-04-13 09:08:54 -0500

    feat(tabnavigation): implementacion de diseño para los módulos

--- Frontend-movil/src/navigation/TabNavigator.tsx
+++ Frontend-movil/src/navigation/TabNavigator.tsx
@@ -1,24 +1,45 @@
+import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from '@expo/vector-icons'; 
+import { SafeAreaView } from "react-native-safe-area-context";
 
+// Importamos pantallas
 import Inicio from "../screens/tabs/inicio";
 import Mapa from "../screens/tabs/mapa/MapaView";
 import Contactos from "../screens/tabs/contactos/contactos";
 import Historial from "../screens/tabs/historialAlerta/historial";
 import Alerta from "../screens/tabs/alerta";
 
+// Importamos el diseño separado
+import { tabScreenOptions } from "../components/ui/Tabs/TabsNavegationStyle";
+
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
   return (
-    <Tab.Navigator 
-      // Esta línea quita la barra blanca superior en todas las pestañas
-      screenOptions={{ headerShown: false }} 
-    >
-      <Tab.Screen name="Inicio" component={Inicio} />
-      <Tab.Screen name="Alerta" component={Alerta} />
-      <Tab.Screen name="Mapa" component={Mapa} />
-      <Tab.Screen name="Contactos" component={Contactos} />
-      <Tab.Screen name="Historial" component={Historial} />
-    </Tab.Navigator>
+    
+      <Tab.Navigator 
+        screenOptions={({ route }) => ({
+          ...tabScreenOptions, // Esparcimos el diseño aquí
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName: React.ComponentProps<typeof Ionicons>['name'] = 'help-circle';
+
+            if (route.name === 'Inicio') iconName = focused ? 'home' : 'home-outline';
+            else if (route.name === 'Alerta') iconName = focused ? 'warning' : 'warning-outline';
+            else if (route.name === 'Mapa') iconName = focused ? 'map' : 'map-outline';
+            else if (route.name === 'Contactos') iconName = focused ? 'people' : 'people-outline';
+            else if (route.name === 'Historial') iconName = focused ? 'time' : 'time-outline';
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Inicio" component={Inicio} />
+        <Tab.Screen name="Mapa" component={Mapa} />
+        <Tab.Screen name="Alerta" component={Alerta} />
+        <Tab.Screen name="Contactos" component={Contactos} />
+        <Tab.Screen name="Historial" component={Historial} />
+      </Tab.Navigator>
+    
   );
 }
