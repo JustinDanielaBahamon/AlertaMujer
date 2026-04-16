5331209620a43ef129346f513752a57e6431bfad
JustinDanielaBahamon <dannybahamon613@gmail.com>
2026-04-16 00:38:15 -0500

    refactor: ajustar ContactosScreen para compatibilidad con paginación y  mejora de diseño

--- Frontend-movil/features/tutorial/contactoScreen.tsx
+++ Frontend-movil/features/tutorial/contactoScreen.tsx
@@ -5,46 +5,45 @@ import { styles } from "./universalStyle";
 import { styles as cardStyles } from "../../src/components/ui/card/cardStyle";
 import Card from "../../src/components/ui/card/card";
 
-export default function ActivacionTutorial() {
+export default function ContactosScreen() {
   return (
     <SafeAreaView style={styles.container} edges={["top", "bottom"]}>
       
-      {/* 1. ILUSTRACIÓN TOTALMENTE FUERA DE LA CARD */}
-      <View style={styles.illustrationWrapper}>
-        <Image
-          source={require("../../assets/imagesAlertaMujer/ScTutorial/Tuto-boton.png")} 
-          style={styles.mainIllustration}
-          resizeMode="contain"
-        />
-      </View>
+      {/* 1. HEADER CON LA IMAGEN (Estructura idéntica al botón) */}
+       <View style={[styles.img, { width: '100%', alignItems: 'center', justifyContent: 'center',marginBottom: -30 }]}>
+          <Image
+            source={require("../../assets/imagesAlertaMujer/ScTutorial/Contacto.png")} 
+            style={{ height: 350, width: 350}} // Tus medidas manuales
+            resizeMode="contain"
+          />
+        </View>
 
-      {/* 2. TARJETA INDEPENDIENTE */}
+      {/* 2. TARJETA DE RED DE APOYO */}
       <Card 
-        title={`Cómo funciona el\nbotón`}
+        title={`Tu Red de\nApoyo`}
         style={cardStyles.card}
       >
-        {/* Contenedor interno con espaciado entre puntos */}
-        <View style={{ gap: 20 }}>
+        <View style={{ gap: 10 }}>
           
           <Text style={cardStyles.description}>
-            <Text style={{ fontWeight: "bold", color: "#fffb00" }}>● Toque corto: </Text>
-            <Text style={{ fontWeight: "bold" }}>Alerta discreta.</Text> Envía un SMS silencioso con tu ubicación.
+            <Text style={{ fontWeight: "bold", color: "#f5caef" }}>● Confianza: </Text>
+            <Text style={{ fontWeight: "bold" }}>Red segura.</Text> Agrega a tus familiares y amigos cercanos para que te cuiden.
           </Text>
 
           <Text style={cardStyles.description}>
-            <Text style={{ fontWeight: "bold", color: "#FFD700" }}>● Doble toque: </Text>
-            <Text style={{ fontWeight: "bold" }}>Alerta urgente.</Text> Tus contactos reciben notificación y llamada.
+            <Text style={{ fontWeight: "bold", color: "#f5caef" }}>● Notificaciones: </Text>
+            <Text style={{ fontWeight: "bold" }}>Aviso inmediato.</Text> Ellos recibirán tu ubicación exacta cuando actives una alerta.
           </Text>
 
           <Text style={cardStyles.description}>
-            <Text style={{ fontWeight: "bold", color: "#FFD700" }}>● Mantener: </Text>
-            <Text style={{ fontWeight: "bold" }}>Alerta máxima.</Text> Inicia grabación, sirena y ubicación en tiempo real.
+            <Text style={{ fontWeight: "bold", color: "#f5caef" }}>● Gestión: </Text>
+            <Text style={{ fontWeight: "bold" }}>Siempre conectada.</Text> Puedes gestionar tus contactos en cualquier momento desde tu perfil.
           </Text>
 
         </View>
       </Card>
 
-      {/* Espaciador flexible para empujar el contenido hacia arriba y dejar aire para la paginación */}
+      {/* Espaciador para mantener la simetría con la paginación */}
       <View style={{ flex: 1 }} />
       
     </SafeAreaView>
