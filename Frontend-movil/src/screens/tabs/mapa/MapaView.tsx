f5449594f61c95d6957a0b5e43b7629ef1255338
JoseSebastianSuarezCumaco <masquebugs1@gmail.com>
2026-04-20 09:16:20 -0500

    Revert "fix(map): corrige multiples pings y mejora diseño visual"
    This reverts commit 0685c11b053cd3e363627b0cc996de5ea96739e7.

--- Frontend-movil/src/screens/tabs/mapa/MapaView.tsx
+++ Frontend-movil/src/screens/tabs/mapa/MapaView.tsx
@@ -199,7 +199,7 @@ export default function MapaView() {
           setHistorial((prev) => [...prev, loc]);
         }
 
-      }, 300000); // cada 5 minutos
+      }, 300000);
 
       /*
        limpieza al salir
