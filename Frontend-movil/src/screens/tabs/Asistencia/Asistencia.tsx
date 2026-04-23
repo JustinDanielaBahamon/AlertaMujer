d38d5ddbf7f6e77b8c5e30fb13d5e32e7be858de
Maicol <samaicol777@gmail.com>
2026-04-22 21:13:09 -0500

    creacion de la rama asistencia

---
+++ Frontend-movil/src/screens/tabs/Asistencia/Asistencia.tsx
@@ -0,0 +1,13 @@
+import {View , Text} from 'react-native';
+
+export default function Asistencia(){
+
+    return(
+        <View>
+            <Text>
+            Hola
+            </Text>
+        </View>
+    );
+    
+}
